0eb87892c247caec49e78394cc3335991973e377
Valery Gusev <dexdeus2@gmail.com>
2023-03-14 14:19:12 +0000

    First data extraction

--- src/specter/__init__.py
+++ src/specter/__init__.py
@@ -1,1 +1,7 @@
-print('hi')
+import logging
+import logging.config
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s - %(message)s',
+)
--- src/specter/extract/ExtractAPI.py
+++ src/specter/extract/ExtractAPI.py
@@ -1,21 +1,32 @@
+import re
 import csv
-from typing import List
+import logging
+from typing import List, Optional
 from pathlib import Path
 
 from bs4 import BeautifulSoup
 
-from HTMLHandler import HTMLHandler, HTMLHandlerWeb
+from src.specter.extract.HTMLHandler import HTMLHandler, HTMLHandlerLocal
+
+
+log = logging.getLogger(__name__)
 
 
 class Scraper:
-    def __init__(self, html_targetloc, html_handler: HTMLHandler = HTMLHandlerWeb()):
-        self.html_target = html_targetloc
-        self.html_handler = html_handler
-        self.soup = BeautifulSoup(self.html_handler.get_html(self.html_target), 'html.parser')
-        self.global_rank = None
+    def __init__(self, html_targetloc, html_handler: HTMLHandler = HTMLHandlerLocal()):
+        self.html_target: str = html_targetloc
+        self.html_handler: HTMLHandler = html_handler
+        self.soup: BeautifulSoup = BeautifulSoup(self.html_handler.get_html(self.html_target), 'html.parser')
+        self.global_rank: Optional[str] = None
 
-    def get_global_rank(self):
-        pass
+    def get_global_rank(self) -> Optional[str]:
+        try:
+            global_rank = self.soup.select_one('p.wa-rank-list__value').text.strip()
+            self.global_rank = re.sub(r'\D', '', global_rank)
+        except AttributeError:
+            logging.warning('Global Rank not found.')
+            self.global_rank = None
+        return self.global_rank
 
 
 class ExtractAPI:
@@ -23,23 +34,23 @@ class ExtractAPI:
         self.scrapers = scrapers
         self.data = None
 
-    def extract_data(self, filepath: str = 'data'):
+    def extract_data(self, filepath: str):
         self.get_data()
         self.save_data(filepath=filepath)
 
     def get_data(self):
-        self.data = [
+        self.data = [[
             'Global Rank',
-        ]
+        ]]
         for scraper in self.scrapers:
             row = [
                 scraper.get_global_rank(),
             ]
             self.data.append(row)
 
-    def save_data(self, filepath: str = 'data'):
-        if not Path(filepath).exists():
-            print('Path does not exist')
+    def save_data(self, filepath):
+        if not Path(filepath).parent.exists():
+            log.error('Path does not exist.')
             return
         with open(filepath, 'w', newline='') as f:
             writer = csv.writer(f)
@@ -47,14 +58,22 @@ class ExtractAPI:
 
 
 if __name__ == "__main__":
+    from definitions import ROOT_DIR
+    # locs = [
+    #     'https://www.similarweb.com/website/tryspecter.com/#overview',
+    #     'https://www.similarweb.com/website/byte-trading.com/#overview',
+    #     'https://www.similarweb.com/website/crunchbase.com/#overview',
+    #     'https://www.similarweb.com/website/pitchbook.com/#overview',
+    #     'https://www.similarweb.com/website/stripe.com/#overview',
+    #     'https://www.similarweb.com/website/google.com/#overview',
+    # ]
     locs = [
-        'https://www.similarweb.com/website/tryspecter.com/#overview',
-        'https://www.similarweb.com/website/byte-trading.com/#overview',
-        'https://www.similarweb.com/website/crunchbase.com/#overview',
-        'https://www.similarweb.com/website/pitchbook.com/#overview',
-        'https://www.similarweb.com/website/stripe.com/#overview',
-        'https://www.similarweb.com/website/google.com/#overview',
+        f'{ROOT_DIR}/data/raw/similarweb-byte-trading-com.html',
+        f'{ROOT_DIR}/data/raw/similarweb-crunchbase-com.html',
+        f'{ROOT_DIR}/data/raw/similarweb-google-com.html',
+        f'{ROOT_DIR}/data/raw/similarweb-pitchbook-com.html',
+        f'{ROOT_DIR}/data/raw/similarweb-stripe-com.html',
     ]
-    scrapers_ = [Scraper(loc, HTMLHandlerWeb()) for loc in locs]
+    scrapers_ = [Scraper(loc, HTMLHandlerLocal()) for loc in locs]
     extract_api = ExtractAPI(scrapers_)
-    extract_api.extract_data(filepath='data/processed')
+    extract_api.extract_data(filepath=f'{ROOT_DIR}/data/processed/scrape_data.csv')
